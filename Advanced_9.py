27abbdfe94251652d64c6f382b5913da94795b78
master <puselnyk.nazar@gmail.com>
2020-06-01 17:58:52 +0300

    Changes to Event loop rea

--- Advanced_9.py
+++ Advanced_9.py
@@ -3,9 +3,9 @@
 # response status - 200." В реализации нет ограничений - можно использовать процессы, потоки, асинхронность.
 # Любые вспомагательные механизмы типа Lock, Semaphore, пулы для тредов и потоков.
 import sys
-import asyncio
-import aiohttp
-import datetime
+from asyncio import get_event_loop, ensure_future, wait
+from aiohttp import ClientSession
+from time import time
 import ssl
 
 url_list = ["http://docs.python-requests.org/",
@@ -26,29 +26,23 @@ url_list = ["http://docs.python-requests.org/",
             ]
 
 
-async def do_request(list_of_urls: list):
-     async with aiohttp.ClientSession() as session:
-          for url in list_of_urls:
-               start_time = datetime.datetime.now().microsecond
-               async with session.get(url, ssl=ssl.SSLContext()) as resp:
-
-                    print(f'Resource {url}')
-                    print(f'Response status={resp.status}')
-                    end_time = datetime.datetime.now().microsecond
-                    print(f'Response time {end_time - start_time}')
+async def get_url(session, url):
+    start_time = time()
+    async with session.request("GET", url, ssl=ssl.SSLContext()) as resp:
+        print(f'Resource {url}')
+        print(f'Response status={resp.status}')
+        end_time = time()
+        print(f'Response time {end_time - start_time}')
 
 
+async def make_request(list_of_urls):
+    start_time = time()
+    async with ClientSession() as session:
+        tasks = [ensure_future(get_url(session, url)) for url in list_of_urls]
+        await wait(tasks)
+    end_time = time()
+    print(f'Loop time={end_time - start_time}')
 
 
-
-async def main():
-    print(datetime.datetime.now().strftime("%A, %B %d, %I:%M %p"))
-    print('---------------------------')
-    loop = asyncio.get_running_loop()
-    async with aiohttp.ClientSession(loop=loop) as client:
-        await asyncio.gather(
-               do_request(url_list)
-            )
-        print(f'Loop time={loop.time()}')
-
-asyncio.run(main())
+loop = get_event_loop()
+loop.run_until_complete(make_request(url_list))
